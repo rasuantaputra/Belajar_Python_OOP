18b674f4d47f38471a63ddb9eebc10e798d73e1d
rasuantaputra <you@example.com>
2022-06-13 20:44:08 +0700

    Mempelajari constuctor

---
+++ 2.  Constructor __init__().py
@@ -0,0 +1,21 @@
+class Hero:
+
+    def __init__(self, input_name, input_health, input_power, input_armor) -> None:
+        self.name = input_name
+        self.health = input_health
+        self.power = input_power
+        self.armor = input_armor
+        pass
+
+hero1 = Hero('sniper', 50, 400, 100)
+hero2 = Hero('Mirana', 100, 100, 350)
+hero3 = Hero('Ucup', 200, 170, 180)
+
+print(hero1.name)
+print(hero2.power)
+print(hero3.armor)
+
+# untuk mengatahui atribut apa saja yang dimiliki
+# print(hero1.__dict__)
+# print(hero2.__dict__)
+# print(hero3.__dict__)
